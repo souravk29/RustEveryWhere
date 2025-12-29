7c2f4fd744082b776839ddaf7d77263a0e0e0061
souravk29 <sk2661901@gmail.com>
2025-12-30 02:58:22 +0530

    added

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
-fn main() {
 
-    let a : u16 = 65535;
-    let ptr = &a;
 
-    println!("{}", a);
-    println!("{}", ptr);
+fn sendHere(s: &String){
+    println!("{} added to the 2nd function", s);
+}
 
+fn main(){
+
+    let a = String::from("this is to be");
+    let b = sendHere(&a);
+
+    // print!("{}", b)
 
 }
