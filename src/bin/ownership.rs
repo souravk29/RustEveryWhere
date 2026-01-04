1d71a29861910b82786da37ce5d3f01eb2114429
souravk29 <sk2661901@gmail.com>
2026-01-05 02:34:25 +0530

    implementing ownership

--- src/bin/ownership.rs
+++ src/bin/ownership.rs
@@ -12,14 +12,54 @@ fn main(){
 
     let literal:&str = "hello world !!";                    // immutable but very fast because hardcoded and the size required is known at compile time
 
-    let Strr: String = String::from("this is a string");
+
+    /*************************************************/
+
+    let mut  Strr: String = String::from("this is a string");
     let reference = find_length(&Strr);
+    let modif = change(&mut Strr);
+    let modif2 = change1(&mut Strr);   // not allowed
+
+    println!("{:?} {:?}", modif, modif2);
 
     println!("{reference}");
+    println!("{Strr}");
 
     fn find_length(a: &String) -> usize{
         a.len()
     }
+
+    fn change(c: &mut String){
+        c.push_str(" immutable");
+
+    }
+
+
+    fn change1(d: &mut String) {
+        d.push_str(" immutability test");
+
+    }
+
+
+    let mut p = String::from("distro");
+    let q = &mut p;
+    let r = &mut p;
+
+   // println!("{q}, {r}")
+
+
+    let mut s5 = String::from("hello");
+
+    let r1 = &s5; // no problem
+    let r2 = &s5; // no problem
+    println!("{r1} and {r2}");
+    println!("{r1} and {r2}");
+    // Variables r1 and r2 will not be used after this point.
+
+    let r3 = &mut s5; // no problem
+    println!("{r3}");
+
+
 
 
 
