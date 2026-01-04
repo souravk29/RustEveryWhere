4c147c47b113940cabe2a824a1e82e63a6cf1a3b
souravk29 <sk2661901@gmail.com>
2026-01-04 22:12:53 +0530

    updated

--- src/bin/control_flow_2.rs
+++ src/bin/control_flow_2.rs
@@ -5,4 +5,31 @@ fn main(){
         println!("{}", num);
         num -= 1;
     }
+
+    println!(" ");
+    println!(" ");
+    println!(" ");
+
+    let array:[i32; 5] = [1,2,3,4,5];
+
+    let mut i = 0;
+    while i<=4 {
+        println!("{}", array[i]);
+        i+=1;
+    }
+
+
+    println!(" ");  println!(" ");  println!(" ");
+
+    for element in array{
+        println!("{element}");
+    }
+    println!(" "); println!(" "); println!(" ");
+
+    for element in (0..=4){
+        println!("{}" , array[element]);
+    }
+
+
+
 }
