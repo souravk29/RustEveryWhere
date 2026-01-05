4a67dcbe28744bf6b9f6c17012dcf16bec4ff8f8
souravk29 <sk2661901@gmail.com>
2026-01-06 01:09:23 +0530

    METHODS IS YET TO BE DONE...

--- src/bin/structs_2.rs
+++ src/bin/structs_2.rs
@@ -1,9 +1,16 @@
+
+#[derive(Debug)]
+struct dim_of_rec{
+    leng: u32,
+    breadth: u32,
+}
+
+
 fn main(){
     let length: u32 = 5;
     let width: u32 = 6;
 
     println!("the area of a rectangle of length {length} and width {width} is : {}", calculate_area(length, width) );
-
 
     fn calculate_area(l: u32, w: u32) -> u32 {
         l*w
@@ -19,7 +26,20 @@ fn main(){
         d.0 * d.1
     }
 
+    /****************************************************************/
 
+    let values_of_dim = dim_of_rec{
+        leng: 10,
+        breadth: 5,
+    };
 
+    println!("{values_of_dim:?}");
 
+    println!("the area of a rectangle of length {length} and width {width} is : {}", calculate_area_by_value_of_dim(&values_of_dim) );
+
+    fn calculate_area_by_value_of_dim( D: &dim_of_rec ) -> u32{
+        D.leng * D.breadth
+    }
 }
+
+/********************************   METHODS: TODO     ******************************/
