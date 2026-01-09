33da6be217f3afedb91c79b494465228e482eede
souravk29 <sk2661901@gmail.com>
2026-01-09 23:57:12 +0530

    implemented strings

--- src/bin/Strings.rs
+++ src/bin/Strings.rs
@@ -12,9 +12,29 @@ fn main() {
     let s2 = String::from("ning");
     let s3 = String::from(" of main");
 
+    let s4 = String::from("Здравствуйте");
+
+    for c in s4.chars(){
+        println!("{c}");
+    }
+
+    println!("");
+
+    for b in s4.bytes(){
+        print!("{b} ");
+    }
+
+    println!("");
+
+    for b in s1.bytes(){
+        print!("{b} ");
+    }
+
     let output_after_format = format!("{s1}{s2}{s3}");
 
-    print!("{output_after_format}");
+   // print!("{output_after_format}");
 
+    let l = s4.len();
+  //  println!("{l}");
 
 }
